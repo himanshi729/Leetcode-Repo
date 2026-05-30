e37ddcd38d97d5b1759b502382c2df5936c2b3ce
himanshi729 <pawarhimanshi913@gmail.com>
2026-05-30 23:49:25 +0530

    Create countingBit.c

---
+++ countingBit.c
@@ -0,0 +1,12 @@
+int* countBits(int n, int* returnSize) {
+    *returnSize = n+1;
+
+    int* ans = (int*)malloc((n+1) * sizeof(int));
+
+    ans[0] = 0;
+
+    for(int i=0; i<=n; i++){
+        ans[i] = ans[i>>1]+(i&1);
+    }
+    return ans;
+}
