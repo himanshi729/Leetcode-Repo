4ad464c2c08e8e2906eb06748139f88d34c31b72
himanshi729 <pawarhimanshi913@gmail.com>
2026-06-03 23:08:16 +0530

    Create summaryRanges.c

---
+++ summaryRanges.c
@@ -0,0 +1,33 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** summaryRanges(int* nums, int numsSize, int* returnSize) {
+      char **result = (char **)malloc(numsSize * sizeof(char *));
+    *returnSize = 0;
+
+    int i = 0;
+
+    while (i < numsSize) {
+
+        int start = nums[i];
+
+        while (i < numsSize - 1 && nums[i + 1] == nums[i] + 1) {
+            i++;
+        }
+
+        int end = nums[i];
+
+        result[*returnSize] = (char *)malloc(30 * sizeof(char));
+
+        if (start == end) {
+            sprintf(result[*returnSize], "%d", start);
+        } else {
+            sprintf(result[*returnSize], "%d->%d", start, end);
+        }
+
+        (*returnSize)++;
+        i++;
+    }
+
+    return result;
+}
